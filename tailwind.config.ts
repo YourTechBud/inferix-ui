af40387be1d71eb350e0a28ee42d13ca9e61450d
maishaSupritee <maisha.sup@gmail.com>
2024-07-18 13:44:31 -0700

    Edit ui components to allow custom styling
    Signed-off-by: Maisha Supritee Chowdhury <maisha.sup@gmail.com>

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -27,6 +27,8 @@ const config = {
         primary: {
           DEFAULT: 'hsl(var(--primary))',
           foreground: 'hsl(var(--primary-foreground))',
+          hover: 'hsl(var(--primary-hover))',
+          green: 'hsl(var(--primary-green))',
         },
         secondary: {
           DEFAULT: 'hsl(var(--secondary))',
