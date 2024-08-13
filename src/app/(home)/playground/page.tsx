c7f24ab51e8c941b94f8184d2b56a981f5f92055
maishaSupritee <maisha.sup@gmail.com>
2024-08-12 20:46:28 -0700

    Create playground(no models) page ui, update button, make new headings
    Signed-off-by: maishaSupritee <maisha.sup@gmail.com>

--- src/app/(home)/playground/page.tsx
+++ src/app/(home)/playground/page.tsx
@@ -1,7 +1,26 @@
+import Image from 'next/image';
+
+import { Button } from '@/ui/components/button';
+import { Heading, SecondaryText, Subheading } from '@/ui/components/heading';
+
 export default function Playground() {
   return (
-    <div className="flex min-w-full items-center justify-center rounded-lg bg-white font-sans antialiased">
-      <h1>Chat</h1>
+    <div className="flex h-full flex-col">
+      <Heading text="Chat" />
+      <div className="flex flex-grow flex-col items-center justify-center gap-4 sm:gap-6">
+        <Image
+          src="/assets/emptyPlayground.png"
+          alt="playground"
+          width={150}
+          height={190}
+          className="max-h-[30vh] w-24 object-contain sm:w-32 md:w-auto"
+        />
+        <div className="flex flex-col items-center gap-2.5 text-center">
+          <Subheading text="You're Just One Step Away From AI-ing!" />
+          <SecondaryText text="You will need to setup connections to Grow, OpenAI or any supported backend to use the playground" />
+        </div>
+        <Button>Setup Connections</Button>
+      </div>
     </div>
   );
 }
