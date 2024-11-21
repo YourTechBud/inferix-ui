ed60f3b13d2c1e2677c0c24929f4ab6cb461172b
maishaSupritee <maisha.sup@gmail.com>
2024-11-21 12:16:19 -0800

    Refactor heading components to make more variants and update usage across all pages
    Signed-off-by: maishaSupritee <maisha.sup@gmail.com>

--- src/app/(home)/api-keys/page.tsx
+++ src/app/(home)/api-keys/page.tsx
@@ -1,13 +1,13 @@
 import Image from 'next/image';
 
 import { Button } from '@/ui/components/button';
-import { Heading, SecondaryText, Subheading } from '@/ui/components/heading';
+import { Heading } from '@/ui/components/headings';
 import PagePanel from '@/ui/components/page-panel';
 
 export default function APIKeys() {
   return (
     <PagePanel className="flex h-full flex-col">
-      <Heading text="API Keys" />
+      <Heading variant="page" text="API Keys" />
       <div className="flex flex-grow flex-col items-center justify-center gap-4 sm:gap-6">
         <Image
           src="/assets/apiKeys.png"
@@ -17,8 +17,14 @@ export default function APIKeys() {
           className="h-auto w-24 object-contain sm:w-[200px]"
         />
         <div className="flex flex-col items-center gap-1 text-center">
-          <Subheading text="Want Programmatic Access? No Problem!" />
-          <SecondaryText text="Manage your API keys. Remember to keep your API keys safe to prevent unauthorized access." />
+          <Heading
+            variant="section"
+            text="Want Programmatic Access? No Problem!"
+          />
+          <Heading
+            variant="secondary"
+            text="Manage your API keys. Remember to keep your API keys safe to prevent unauthorized access."
+          />
         </div>
         <Button>Create API Key</Button>
       </div>
