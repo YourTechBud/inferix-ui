d7538e7ded5f146f35267af0beaa818299bb4b2d
maishaSupritee <maisha.sup@gmail.com>
2024-11-20 21:58:42 -0800

    Add orientation prop to Divider component and create ChatMessage component for playground
    Signed-off-by: maishaSupritee <maisha.sup@gmail.com>

--- src/ui/components/divider.tsx
+++ src/ui/components/divider.tsx
@@ -2,15 +2,19 @@ import clsx from 'clsx';
 
 export function Divider({
   soft = false,
+  orientation = 'horizontal',
   className,
   ...props
-}: { soft?: boolean } & React.ComponentPropsWithoutRef<'hr'>) {
+}: {
+  soft?: boolean;
+  orientation?: string;
+} & React.ComponentPropsWithoutRef<'hr'>) {
   return (
     <hr
       {...props}
       className={clsx(
         className,
-        'w-full border-t',
+        orientation == 'vertical' ? 'h-full border-l' : 'w-full border-t',
         soft && 'border-zinc-950/5 dark:border-white/5',
         !soft && 'border-zinc-950/10 dark:border-white/10',
       )}
