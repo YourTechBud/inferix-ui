c87571ff2269a30d24ac24e3a08909209a6f5b25
Stephen Werronen <stephen.werronen@gmail.com>
2024-12-06 20:04:15 -0800

    Add base url (#22)
    Signed-off-by: Stephen Werronen <stephen.werronen@gmail.com>

--- src/ui/widgets/api-keys/APIKeysPage.tsx
+++ src/ui/widgets/api-keys/APIKeysPage.tsx
@@ -1,4 +1,9 @@
+import { Copy } from 'lucide-react';
+import { useEffect, useState } from 'react';
+import { toast } from 'sonner';
+
 import { APIKey } from '@/app/(home)/api-keys/page';
+import { Button } from '@/ui/components/button';
 import APIKeyTable from '@/ui/widgets/api-keys/APIKeyTable';
 
 type APIKeysPageProps = {
@@ -10,12 +15,49 @@ export default function APIKeysPage({
   apiKeys,
   removeAPIKey,
 }: APIKeysPageProps) {
+  const [baseUrl, setBaseUrl] = useState('');
+
+  const handleCopy = async () => {
+    await navigator.clipboard.writeText(baseUrl);
+    toast.success('Base URL copied to clipboard!');
+  };
+
+  useEffect(() => {
+    const { protocol, hostname, port } = window.location;
+
+    const isHttpsDefaultPort = protocol === 'https' && port === '443';
+    const isHttpDefaultPort = protocol === 'http' && port === '80';
+    const isDefaultPort = isHttpsDefaultPort || isHttpDefaultPort;
+
+    setBaseUrl(
+      `${protocol}//${hostname}${isDefaultPort ? '' : ':' + port}/inferix/v1/llm`,
+    );
+  }, []);
+
   return (
     <>
       <p>
         Manage your API keys. Remember to keep your API keys safe to prevent
         unauthorized access.
       </p>
+      <div className="mt-4 flex items-center gap-2">
+        <p className="py-2">Base URL:</p>
+        {baseUrl && (
+          <>
+            <div className="ml-1 flex items-center gap-1 rounded-lg border pl-4">
+              <p>{baseUrl}</p>
+              <Button
+                plain
+                size="icon"
+                aria-label="Copy base URL"
+                onClick={handleCopy}
+              >
+                <Copy />
+              </Button>
+            </div>
+          </>
+        )}
+      </div>
       <APIKeyTable apiKeys={apiKeys} removeAPIKey={removeAPIKey} />
     </>
   );
