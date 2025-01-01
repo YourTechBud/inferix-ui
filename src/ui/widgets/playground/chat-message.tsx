ea936dc47cfef6373b0430e6430238c35c6eb8a5
maishaSupritee <maisha.sup@gmail.com>
2024-12-31 23:29:42 -0800

    Refactor ChatMessage component to use textarea for input and adjust heights dynamically
    Signed-off-by: maishaSupritee <maisha.sup@gmail.com>

--- src/ui/widgets/playground/chat-message.tsx
+++ src/ui/widgets/playground/chat-message.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { useRef, useState } from 'react';
 
 import { cn } from '@/lib/utils';
 
@@ -9,32 +10,56 @@ interface ChatMessageProps extends React.InputHTMLAttributes<HTMLInputElement> {
 
 export default function ChatMessage({
   className,
-  type,
-  variant,
-  ...props
+  variant = 'assistant'
 }: ChatMessageProps) {
-  const inputRef = React.useRef<HTMLInputElement>(null);
+  const textareaRef = useRef<HTMLTextAreaElement>(null);
+  const divRef = useRef<HTMLDivElement>(null);
 
   const inputClassName = cn(
-    'flex h-10 rounded-md bg-white px-3 pt-2 pb-6 text-sm placeholder:text-muted-foreground focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50 pt-12',
-    variant === 'system' && 'border border-input',
+    'w-full text-sm placeholder:text-muted-foreground border-none focus-visible:outline-none resize-none overflow-hidden disabled:cursor-not-allowed disabled:opacity-50 h-10 pt-6 pb-6',
     className,
   );
+  const divClassName = cn('relative resize-none overflow-hidden min-h-10 max-h-40 rounded-md bg-white bg-white ring-offset-background focus-visible:outline-none px-3 pt-2 pb-4', 
+    variant === 'system' && 'border border-input', className);
 
+  const [message, setMessage] = useState('');
+  const adjustHeights = () => {
+    if (textareaRef.current) {
+      textareaRef.current.style.height = 'auto';
+      textareaRef.current.style.overflow = 'auto';
+      textareaRef.current.style.height = `${textareaRef.current.scrollHeight}px`;
+    }
+    if (divRef.current && textareaRef.current) {
+      divRef.current.style.height = 'auto';
+      divRef.current.style.overflow = 'auto';
+      divRef.current.style.height = `${
+        textareaRef.current.scrollHeight + 12 //adding extra space for buttons
+      }px`;
+    }
+  };
+
+  const handleInput = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setMessage(e.target.value);
+    adjustHeights();
+  };
+  
   return (
-    <div className="relative">
+    <div className={divClassName} ref={divRef}>
       <p className="absolute left-0 top-0 px-3 py-2 text-base font-medium text-zinc-700">
         {(variant === 'system' && 'SYSTEM') ||
           (variant === 'user' && 'USER') ||
           'ASSISTANT'}
       </p>
 
-      <input
-        type={type}
+      <textarea
+        id="message"
         className={inputClassName}
-        ref={inputRef}
-        {...props}
+        ref={textareaRef}
+        value={message}
+        rows={1}
         placeholder={variant === 'system' ? 'Enter System Instructions' : ''}
+        onChange={handleInput}
+        aria-multiline="true"
       />
     </div>
   );
