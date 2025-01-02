d5bcfd0ecee86059ab47ccb415239e3a224cf909
maishaSupritee <maisha.sup@gmail.com>
2025-01-01 21:53:41 -0800

    Adjust textarea height and separate chatMessage components in divs
    Signed-off-by: maishaSupritee <maisha.sup@gmail.com>

--- src/ui/widgets/playground/chat-message.tsx
+++ src/ui/widgets/playground/chat-message.tsx
@@ -16,25 +16,25 @@ export default function ChatMessage({
   const divRef = useRef<HTMLDivElement>(null);
 
   const inputClassName = cn(
-    'w-full text-sm placeholder:text-muted-foreground border-none focus-visible:outline-none resize-none overflow-hidden disabled:cursor-not-allowed disabled:opacity-50 h-10 pt-6 pb-6',
+    'w-full text-sm placeholder:text-muted-foreground border-none focus-visible:outline-none resize-none disabled:cursor-not-allowed disabled:opacity-50',
     className,
   );
+
   const divClassName = cn(
-    'flex flex-col relative resize-none overflow-hidden min-h-10 max-h-24 rounded-md bg-white bg-white ring-offset-background focus-visible:outline-none px-3 pt-2 pb-4',
+    'flex flex-col relative min-h-[64px] rounded-md bg-white ring-offset-background focus-visible:outline-none',
     variant === 'system' && 'border border-input',
     className,
   );
 
   const [message, setMessage] = useState('');
+
   const adjustHeights = () => {
     if (textareaRef.current) {
       textareaRef.current.style.height = 'auto';
-      textareaRef.current.style.overflow = 'auto';
-      textareaRef.current.style.height = `${textareaRef.current.scrollHeight}px`;
-    }
-    if (divRef.current && textareaRef.current) {
-      divRef.current.style.height = 'auto';
-      divRef.current.style.height = `${textareaRef.current.scrollHeight}px`;
+      const newHeight = Math.min(textareaRef.current.scrollHeight, 64);
+      textareaRef.current.style.height = `${newHeight}px`;
+      textareaRef.current.style.overflowY =
+        textareaRef.current.scrollHeight > 40 ? 'auto' : 'hidden';
     }
   };
 
@@ -45,22 +45,26 @@ export default function ChatMessage({
 
   return (
     <div className={divClassName} ref={divRef}>
-      <p className="absolute left-0 top-0 px-3 py-2 text-base font-medium text-zinc-700">
-        {(variant === 'system' && 'SYSTEM') ||
-          (variant === 'user' && 'USER') ||
-          'ASSISTANT'}
-      </p>
+      <div className="sticky left-0 top-0 w-full px-3 py-2">
+        <p className="text-base font-medium text-zinc-700">
+          {(variant === 'system' && 'SYSTEM') ||
+            (variant === 'user' && 'USER') ||
+            'ASSISTANT'}
+        </p>
+      </div>
 
-      <textarea
-        id="message"
-        className={inputClassName}
-        ref={textareaRef}
-        value={message}
-        rows={1}
-        placeholder={variant === 'system' ? 'Enter System Instructions' : ''}
-        onChange={handleInput}
-        aria-multiline="true"
-      />
+      <div className="px-3 pb-2">
+        <textarea
+          id="message"
+          className={inputClassName}
+          ref={textareaRef}
+          value={message}
+          rows={1}
+          placeholder={variant === 'system' ? 'Enter System Instructions' : ''}
+          onChange={handleInput}
+          aria-multiline="true"
+        />
+      </div>
     </div>
   );
 }
