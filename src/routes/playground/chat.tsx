fd3cd8e4c2b162ed2763f8f28872124afb09d90b
maishaSupritee <maisha.sup@gmail.com>
2025-01-13 19:46:16 -0800

    Fix chat layout ui, and remove height adjustment from chatMessage
    Signed-off-by: maishaSupritee <maisha.sup@gmail.com>

--- src/routes/playground/chat.tsx
+++ src/routes/playground/chat.tsx
@@ -105,8 +105,8 @@ export default function Component() {
   return (
     <PagePanel className="flex h-full flex-col">
       <Heading variant="page" text="Chat" />
-      <div className="mb-4 mt-4 flex flex-grow flex-row gap-4 max-h-[80vh] sm:gap-6 lg:mb-0">
-        <div className="flex h-full flex-grow flex-col justify-between sm:w-full md:w-[25vw] lg:w-[45vw]">
+      <div className="mb-4 mt-4 flex max-h-[63vh] flex-grow flex-row gap-4 sm:gap-6 lg:mb-0 lg:max-h-[70vh]">
+        <div className="flex w-[60vw] flex-grow flex-col justify-between md:w-[25vw] lg:w-[45vw]">
           <div className="flex h-full flex-grow flex-col gap-4">
             <div className="flex flex-row gap-4">
               <CustomDropdown
